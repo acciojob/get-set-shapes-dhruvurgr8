47eab1b15953310006b4939dc602eb6286297c70
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-22 14:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width;
+		this.height = height;
+	}
+	set width(width){
+		this.width = width;
+	}
+	set height(height){
+		this.height = height;
+	}
+	get width(){
+		return this.width
+	}
+	get height(){
+		return this.height
+	}
+	getArea(){
+		const res = (this.height*this.width);
+		return res;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		super(height,width);
+	}
+	getPerimeter(){
+		const res = this.side*4;
+		return res;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
